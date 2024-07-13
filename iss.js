b0bd5356e2c96f1775635c82f5489397ff401a38
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-13 18:26:08 -0400

    Implemented a fetch my ip function that gets your ip in json format

---
+++ iss.js
@@ -0,0 +1,32 @@
+const needle = require("needle");
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  needle.get('https://api.ipify.org?format=json', (error, response, ip) => {
+    callback(error, ip);
+    // inside the request callback ...
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${ip}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
+  });
+};
+
+module.exports = { fetchMyIP };
